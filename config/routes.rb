3b29d6ec25c88356fcc64883a8740e2dcfcc640f
neil patel <neil@myindianweddingapp.com>
2014-02-14 21:55:57 +0000

    dynamic home page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ LearnRails::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+root to: "visitors#new"
+
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
