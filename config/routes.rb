b472314a7f0ed41405fb48bc768336e31cb39972
neil patel <neil@myindianweddingapp.com>
2014-02-17 20:56:14 +0000

    contact form

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 LearnRails::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+resources :contacts, only: [:new, :create]
 root to: "visitors#new"
 
   # You can have the root of your site routed with "root"
